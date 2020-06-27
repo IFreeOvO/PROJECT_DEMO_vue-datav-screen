dc7f54f1ec87d67d1790764fa4ec2395dadeafb3
chenzihao <278199196@qq.com>
2020-06-27 22:16:54 +0800

    chapter 10-3：vue3基本开发体验

---
+++ src/views/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: '',
+
+  data() {
+    return {
+      msg: '测试'
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
