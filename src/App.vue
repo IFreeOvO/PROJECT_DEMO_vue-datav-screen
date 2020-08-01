bcdd41937bf3e8265c83cc78a35db305445600a4
chenzihao <278199196@qq.com>
2020-08-01 21:55:56 +0800

    chapter 16-3：数据大屏整体布局开发

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/test">Test</router-link>
-      <div class="icon">
-        <test-component></test-component>
-      </div>
-      <svg width="50" height="50">
+    <!-- <svg width="50" height="50">
         <use href="#more"></use>
       </svg>
       <svg width="50" height="50">
@@ -15,37 +8,48 @@
       </svg>
       <svg width="50" height="50">
         <use href="#arrowRight"></use>
-      </svg>
-    </div>
-    <router-view/>
+      </svg> -->
+
+    <!-- <Icon name="arrowRight" :style="{width: 50, height: 50}"></Icon> -->
+
+    <!-- <svg-animation></svg-animation> -->
+
+    <!-- <imooc-loading
+        width="500"
+        height="200"
+        duration='1.5'
+      >加载中...</imooc-loading>
+
+      <div class="box">
+        <imooc-fly-box starLength="200" duration="3">
+          <div>111</div>
+          <div>111</div>
+          <div>111</div>
+          <div>111</div>
+          <div>111</div>
+          <div>111</div>
+        </imooc-fly-box>
+      </div>
+
+      <div class="box1">
+        <imooc-fly-box starLength="200" duration="3">
+          <div>111</div>
+          <div>111</div>
+          <div>111</div>
+          <div>111</div>
+          <div>111</div>
+          <div>111</div>
+        </imooc-fly-box>
+      </div> -->
+
+    <router-view></router-view>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
-.icon {
-  color: green;
-  height: 100px;
-  width: 100px;
+html, body, #app {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,194 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <imooc-loading v-if="loading">
+      <div class="loading-text">数据大屏加载中...</div>
+    </imooc-loading>
+    <imooc-container
+      :options="{width:3840,height:2160}"
+      v-else
+    >
+      <div class="header">111</div>
+      <div class="separator">222</div>
+      <div class="center">
+        <div class="left">
+          <div class="left1">333</div>
+          <div class="left2">444</div>
+          <div class="left3">555</div>
+          <div class="left4">666</div>
+          <div class="left5">777</div>
+          <div class="left6">888</div>
+        </div>
+        <div class="right">
+          <div class="right-top1">999</div>
+          <div class="right-top2">000</div>
+          <div class="right-bottom">
+            <div class="right-left">
+              <div class="right-left1">111</div>
+              <div class="right-left2">222</div>
+              <div class="right-left3">333</div>
+              <div class="right-left4">444</div>
+            </div>
+            <div class="right-right">
+              <div class="right-right1">555</div>
+              <div class="right-right2">666</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </imooc-container>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script type="text/ecmascript-6">
+import { ref, onMounted } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+
+  setup() {
+    const loading = ref(true)
+
+    onMounted(() => {
+      setTimeout(() => {
+        loading.value = false
+      }, 2000)
+    })
+
+    return {
+      loading
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  height: 100%;
+  width: 100%;
+  background: rgb(29, 29, 29);
+  color: #fff;
+  font-size: 48px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  #imooc-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  .header {
+    height: 167px;
+    background: yellow;
+    width: 100%;
+  }
+  .separator {
+    height: 10px;
+    background: #000;
+    width: 100%;
+  }
+  .center {
+    flex: 1;
+    background: rebeccapurple;
+    width: 100%;
+    display: flex;
+    .left {
+      flex: 0 0 860px;
+      background: red;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      padding-bottom: 20px;
+      box-sizing: border-box;
+      .left1 {
+        height: 300px;
+        background: green;
+      }
+      .left2 {
+        height: 320px;
+        background: yellowgreen;
+      }
+      .left3 {
+        height: 280px;
+        background: blueviolet;
+      }
+      .left4 {
+        height: 300px;
+        background: mediumpurple;
+      }
+      .left5 {
+        height: 360px;
+        background: pink;
+      }
+      .left6 {
+        height: 360px;
+        background: darkred;
+      }
+    }
+    .right {
+      flex: 1;
+      background: blue;
+      display: flex;
+      flex-direction: column;
+      .right-top1 {
+        height: 206px;
+        background: darkred;
+      }
+      .right-top2 {
+        height: 48px;
+        background: cadetblue;
+      }
+      .right-bottom {
+        flex: 1;
+        display: flex;
+        padding-bottom: 20px;
+        .right-left {
+          display: flex;
+          flex-direction: column;
+          flex: 0 0 1917px;
+          justify-content: space-between;
+          .right-left1 {
+            height: 999px;
+            background: red;
+          }
+          .right-left2 {
+            height: 80px;
+            background: yellowgreen;
+          }
+          .right-left3 {
+            height: 350px;
+            background: deepskyblue;
+          }
+          .right-left4 {
+            height: 220px;
+            background: orangered;
+          }
+        }
+        .right-right {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          margin-left: 10px;
+          justify-content: space-between;
+          .right-right1 {
+            height: 999px;
+            background: burlywood;
+          }
+          .right-right2 {
+            flex: 1;
+            background: darkred;
+            margin-top: 20px;
+          }
+        }
+      }
+    }
+  }
+}
+
+.loading-text {
+  font-size: 20px;
+  margin-top: 10px;
+}
+</style>
