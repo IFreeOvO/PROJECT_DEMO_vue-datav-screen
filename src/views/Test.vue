968b43bf8198b1f887075b48a1274c33be239c9c
chenzihao <278199196@qq.com>
2020-08-08 22:35:23 +0800

    chapter 17-8：标题动态列宽算法开发

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -21,7 +21,7 @@ export default {
     // return useTest()
     const config = ref({})
     const headerData = ['姓名', '年龄', '月薪']
-    const headerStyle = [{ color: 'red' }]
+    const headerStyle = [{ color: 'red', width: '100px' }]
 
     config.value = {
       headerData,
