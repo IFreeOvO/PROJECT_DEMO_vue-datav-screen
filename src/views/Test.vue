52f1f1f0ac22d1a3db4ec89cbb79a7afa578aef6
chenzihao <278199196@qq.com>
2020-08-08 20:53:36 +0800

    chapter 17-5：基础滑动列表标题样式开发

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,22 +1,41 @@
 <template>
   <div>
-    <div>compunted: {{doubleCount}}</div>
+    <!-- <div>compunted: {{doubleCount}}</div>
     <div>ref: {{refNum}}</div>
     <div>vuex number: {{number}}</div>
     <div>vuex aa: {{a}}</div>
     <button @click="increment">{{state.count}}</button>
     <button @click="gotoAbout">gotoAbout</button>
     <button @click="updateNum">vuex commit</button>
-    <button @click="updateA">vuex commit a</button>
+    <button @click="updateA">vuex commit a</button> -->
+    <base-scroll-list
+      :header="header"
+      :headerStyle="headerStyle"
+      :headerHeight='40'
+    ></base-scroll-list>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import useTest from '@/views/useTest'
+import { ref } from 'vue'
+// import useTest from '@/views/useTest'
 
 export default {
   setup() {
-    return useTest()
+    // return useTest()
+    const header = ref([])
+    const headerStyle = ref([])
+
+    header.value = ['姓名', '年龄', '月薪']
+    headerStyle.value = [{ color: 'red' }]
+
+    return {
+      header,
+      headerStyle
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+</style>
