fac8e7750467e0ebf851b7d83bfa13504ba18365
chenzihao <278199196@qq.com>
2020-08-23 13:30:03 +0800

    chapter 19-1：echart-gl入门--旋转地球基础纹理贴图+背景贴图1

---
+++ src/components/RotatingEarth/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="rotating-earch">
+    <vue-echarts :options="options"></vue-echarts>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import { ref, onMounted } from 'vue'
+import 'echarts-gl'
+
+const ROOT_PATH = './'
+export default {
+  name: 'RotatingEarch',
+
+  setup() {
+    const options = ref({})
+
+    const update = () => {
+      options.value = {
+        globe: {
+          // 地球材质贴图
+          baseTexture: `${ROOT_PATH}assets/datav-gl-texture.jpg`,
+          // 背景贴图
+          environment: `${ROOT_PATH}assets/star-bg.jpg`
+        }
+      }
+    }
+
+    onMounted(() => {
+      update()
+    })
+
+    return {
+      options
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.rotating-earch {
+  height: 100%;
+}
+</style>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,20 +1,28 @@
 <template>
   <div style="width:100%;height:100%">
-    <jiangsu-map-warning></jiangsu-map-warning>
+    <rotating-earth></rotating-earth>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-import { ref } from 'vue'
-import JiangsuMapWarning from '@/components/JiangsuMapWarning/index'
+import { ref, onMounted } from 'vue'
+import RotatingEarth from '@/components/RotatingEarth/index'
 
 export default {
   components: {
-    JiangsuMapWarning
+    RotatingEarth
   },
 
   setup() {
     const options = ref({})
+
+    const update = () => {
+
+    }
+
+    onMounted(() => {
+      update()
+    })
 
     return {
       options
